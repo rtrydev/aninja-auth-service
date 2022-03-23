a3733bae868efe75c37d3b485d7a9535f0788340
rtrydev <r.kulka44@gmail.com>
2022-03-23 14:35:15 +0100

    add jwt token service with getToken method

---
+++ Authorization/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using aninja_auth_service.Models;
+
+namespace aninja_auth_service.Authorization
+{
+    public interface IJwtService
+    {
+        public string? GetJwtToken(User user);
+
+    }
+}
---
+++ Authorization/JwtService.cs
@@ -0,0 +1,38 @@
+﻿using aninja_auth_service.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+
+namespace aninja_auth_service.Authorization
+{
+    public class JwtService : IJwtService
+    {
+        private ILogger<JwtService> _logger;
+        public JwtService(ILogger<JwtService> logger)
+        {
+            _logger = logger;
+        }
+        public string? GetJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET");
+            if(jwtSecret is null)
+            {
+                _logger.LogError("The JWT_SECRET environmental variable has not been set");
+                return null;
+            }
+            var key = Encoding.ASCII.GetBytes(jwtSecret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
