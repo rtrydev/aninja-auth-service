c225725ecbec36ce60bbb335d7a63979484a1601
rtrydev <r.kulka44@gmail.com>
2022-03-23 18:47:30 +0100

    add user repo with mongo

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using aninja_auth_service.Models;
+using MongoDB.Driver;
+
+namespace aninja_auth_service.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private IMongoClient _mongoClient;
+        private IMongoCollection<User> _users;
+
+        public UserRepository(IMongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+            _users = _mongoClient.GetDatabase("usersDB").GetCollection<User>("users");
+        }
+        public async Task CreateUser(User user)
+        {
+            await _users.InsertOneAsync(user);
+        }
+
+        public async Task<User?> GetUserByCreditionals(string username, string password)
+        {
+            var result = await _users.FindAsync<User>(x => x.Name == username && x.Password == password);
+            if(!result.Any()) return null;
+            return await result.FirstAsync();
+        }
+
+        public async Task<User?> GetUserById(Guid id)
+        {
+            var result = await _users.FindAsync<User>(x => x.Id == id);
+            if (!result.Any()) return null;
+            return await result.FirstAsync();
+        }
+
+        public async Task<User?> UpdateUser(User user)
+        {
+            var result = await _users.FindOneAndReplaceAsync<User>(x => x.Id == user.Id, user);
+            return result;
+        }
+    }
+}
